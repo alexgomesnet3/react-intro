d08b184c581ad9bf9b390216715b4afc619a82d6
alexgomesnet3 <alex.gomes.net3@gmail.com>
2019-07-19 02:19:20 -0300

    One file with two components inside

---
+++ src/components/ExampleWithTwoComponents.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ComponentA = props => 
+                        <h1>The Component A say: { props.sayA }</h1>
+const ComponentB = props => 
+                        <h1>The Component B say: { props.sayB }</h1>
+
+export { ComponentA, ComponentB }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import FirstComponent from './components/FirstComponent'
+import { ComponentA, ComponentB } from './components/ExampleWithTwoComponents'
+
 
 
 const elemento = document.getElementById('root')
@@ -9,5 +11,7 @@ ReactDom.render(
         <FirstComponent value="Hello, Good Morning!" 
                         anything="How old are you, please?"
                         ageNumber={2**5}/>
+        <ComponentA sayA="And you Sir. How old are you?"/>
+        <ComponentB sayB="I've 50 years old"/>
     </div>
 , elemento)
